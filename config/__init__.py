f1ad4c14d1904080384d95d0c0034b905b2de47b
kfrime <kfrime@163.com>
2018-01-24 10:53:29 +0800

    add log config and init_app

--- config/__init__.py
+++ config/__init__.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 
-import os
+import logging
 
 from .base import *
 from .apscheduler_config import *
@@ -18,3 +18,13 @@ elif FLASK_CONFIG == 'dev':
     from .dev import *
 else:
     from .dev import *
+
+
+# 自定义的 init_app
+def init_app(app):
+    # 日志系统管理
+    handler = logging.FileHandler(os.path.join(basedir, 'log/app.log'), encoding='UTF-8')
+    logging_format = logging.Formatter(
+        '[%(asctime)s]-[%(levelname)s]-[%(module)s/%(filename)s-%(funcName)s()-%(lineno)s] - %(message)s')
+    handler.setFormatter(logging_format)
+    app.logger.addHandler(handler)
--- explore.py
+++ explore.py
@@ -2,13 +2,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 
-import os
-import logging
 from flask import Flask
 from flask_apscheduler import APScheduler
 
 import config
-from config import basedir
 
 
 scheduler = APScheduler()
@@ -17,6 +14,7 @@ scheduler = APScheduler()
 def create_app():
     app = Flask(__name__)
     app.config.from_object(config)
+    config.init_app(app)
 
     from models import db
     db.app = app
@@ -29,14 +27,6 @@ def create_app():
 
 
 app = create_app()
-
-
-# 日志系统管理
-handler = logging.FileHandler(os.path.join(basedir, 'log/app.log'), encoding='UTF-8')
-logging_format = logging.Formatter(
-            '[%(asctime)s]-[%(levelname)s]-[%(module)s/%(filename)s-%(funcName)s()-%(lineno)s] - %(message)s')
-handler.setFormatter(logging_format)
-app.logger.addHandler(handler)
 
 
 @app.route('/')
