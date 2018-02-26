e5caafc1f8748a56df4162bf75abff63a6de6919
kfrime <kfrime@163.com>
2018-02-26 18:21:03 +0800

    add grammar dir, example 1-1 in <<fluent_python>>

---
+++ grammar/fluent_python/py_1_1_card.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+How to use __len__ and __getitem__
+"""
+
+from __future__ import absolute_import, unicode_literals, print_function
+import collections
+
+
+Card = collections.namedtuple('Card', ['rank', 'suit'])
+
+
+class FrenchDeck(object):
+    ranks = [str(n) for n in range(2, 11)] + list('JQKA')
+    suits = 'spades diamonds clubs hearts'.split()
+
+    def __init__(self):
+        self._cards = [Card(rank, suit) for suit in self.suits
+                                        for rank in self.ranks]
+
+    def __len__(self):
+        return len(self._cards)
+
+    def __getitem__(self, position):
+        return self._cards[position]
+
+
+# sort
+suit_values = dict(spades=3, hearts=2, clubs=1, diamonds=0)
+
+
+def spades_high(card):
+    rank_value = FrenchDeck.ranks.index(card.rank)
+    return rank_value * len(suit_values) + suit_values[card.suit]
+
