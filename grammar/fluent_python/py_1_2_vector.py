63fe30ce0fb1aac0692b54d0a66ee2d75cc8bc98
kfrime <kfrime@163.com>
2018-02-28 18:22:58 +0800

    vector

---
+++ grammar/fluent_python/py_1_2_vector.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals, print_function
+from math import hypot
+
+
+class Vector(object):
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return 'Vector(%r, %r)' % (self.x, self.y)
+
+    def __abs__(self):
+        return hypot(self.x, self.y)
+
+    def __bool__(self):
+        # return bool(abs(self))
+        return bool(self.x or self.y)
+
+    def __add__(self, other):
+        x = self.x + other.x
+        y = self.y + other.y
+        return Vector(x, y)
+
+    def __mul__(self, scalar):
+        return Vector(self.x * scalar, self.y * scalar)
+
